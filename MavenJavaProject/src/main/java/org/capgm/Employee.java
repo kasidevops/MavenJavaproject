5b7c60606991fc72dca58495d208ef38a273d1b0
sruthi213 <srupremsundar@gmail.com>
2020-08-18 11:45:29 +0530

    My first commit

--- MavenJavaProject/src/main/java/org/capgm/Employee.java
+++ MavenJavaProject/src/main/java/org/capgm/Employee.java
@@ -1,14 +1,55 @@
 package org.capgm;
 
 public class Employee {
+	int EmpId;
+	String EmpName;
+	public Employee(int empId, String empName) {
+		super();
+		EmpId = empId;
+		EmpName = empName;
+		
+	}
 	
-	private int empid;
-	private String name;
-	private float basicsal;
-	String skillset;
-	public static void main(String[] args) {
+@Override
+	public boolean equals(Object o) {
 		// TODO Auto-generated method stub
-
+	      Employee e= (Employee)o;
+	      if(this.EmpId==e.EmpId&&this.EmpName.equals(e.EmpName))
+	      {
+	      
+		return true;
+	      }
+	      else
+	      {
+	    	  return false;
+	      }
 	}
 
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return EmpId+EmpName.hashCode();
+	//return 55;
+	}
+
+
+
+	public String toString() {
+		return "Employee [EmpId=" + EmpId + ", EmpName=" + EmpName + "]";
+	}
+
+public static void main(String args[])
+{
+	Employee emp1=new Employee(101,"keeru");
+	Employee emp2=new Employee(103,"shalu");
+	Employee emp3=new Employee(101,"keeru");
+	Employee emp4=new Employee(102,"sru");
+	System.out.println(emp1.EmpId+" "+emp1.EmpName+" "+emp1.hashCode()+" "+System.identityHashCode(emp1));
+	System.out.println(emp2.EmpId+" "+emp2.EmpName+" "+emp2.hashCode()+" "+System.identityHashCode(emp2));
+	System.out.println(emp3.EmpId+" "+emp3.EmpName+" "+emp3.hashCode()+" "+System.identityHashCode(emp3));
+	//System.out.println(emp1.toString());
+	System.out.println(emp1.equals(emp2));
+	System.out.println(emp1.equals(emp3));
+	System.out.println(emp1.equals(emp4));
 }
+}
+

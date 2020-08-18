4d95b04bcb309a94b2ecb71ee6b623cda9b9355b
sruthi213 <srupremsundar@gmail.com>
2020-08-18 11:36:59 +0530

    My first commit

---
+++ MavenJavaProject/src/main/java/org/capgm/Employee.java
@@ -0,0 +1,14 @@
+package org.capgm;
+
+public class Employee {
+	
+	private int empid;
+	private String name;
+	private float basicsal;
+	String skillset;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
